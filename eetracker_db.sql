94d0f5d220dbaf001e917c9a4d070b797fef5b2f
Printice Lawton <printicelaw10@gmail.com>
2021-06-13 14:22:23 -0400

    added primary key to employee table in schema. Table now populates

--- eetracker_db.sql
+++ eetracker_db.sql
@@ -17,7 +17,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT primary key NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10),
