e20f2a04712eb8a2f6649aba4fc6eaeabfaae493
Printice Lawton <printicelaw10@gmail.com>
2021-06-13 13:09:23 -0400

    port working

---
+++ eetracker.js
@@ -0,0 +1,24 @@
+// require dependencies 
+const mysql = require('mysql');
+const inquirer = require("inquirer");
+
+// Establish MYSQL connection 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "printice123",
+    database: "eetracker_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    askQuestions();
+});
