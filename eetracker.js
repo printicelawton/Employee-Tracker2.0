4e3a73d23852239a63133acc0bdbb0630c9fc66b
Printice Lawton <printicelaw10@gmail.com>
2021-06-13 13:49:31 -0400

    working on query functions

--- eetracker.js
+++ eetracker.js
@@ -22,3 +22,102 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId + "\n");
     askQuestions();
 });
+
+// Add a welcome message/function 
+// function init() {
+//     console.log(`Welcome to the Employee Management System (EMS)`)
+//     welcome();
+//   }
+function askQuestions() {
+    inquirer.prompt({
+        message: "what would you like to do?",
+        type: "list",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "Add employee",
+            "Add department",
+            "Add role",
+            "Apdate employee role",
+            "QUIT"
+        ],
+        name: "choice"
+    })
+    
+    .then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "View all employees":
+                viewEmployees()
+                break;
+
+            case "View all departments":
+                viewDepartments()
+                break;
+
+            case "Add employee":
+                addEmployee()
+                break;
+
+            case "Add department":
+                addDepartment()
+                break;
+
+            case "Add role":
+                addRole()
+                break;
+
+            case "Update employee role":
+                updateEmployeeRole();
+                break;
+
+            default:
+                connection.end()
+                break;
+        }
+    });
+};
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", function (err, data) {
+        console.table(data);
+        askQuestions();
+    })
+}
+
+function viewDepartments() {
+    connection.query("SELECT * FROM department", function (err, data) {
+        console.table(data);
+        askQuestions();
+    })
+}
+
+function addEmployee() {
+    inquirer.prompt([{
+            type: "input",
+            name: "firstName",
+            message: "What is the employees first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employees last name?"
+        },
+        {
+            type: "number",
+            name: "roleId",
+            message: "What is the employees role ID"
+        },
+        {
+            type: "number",
+            name: "managerId",
+            message: "What is the employees manager's ID?"
+        }
+    ]).then(function(res) {
+        connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+            if (err) throw err;
+            console.table("Successfully Inserted");
+            askQuestions();
+        })
+    })
+}
